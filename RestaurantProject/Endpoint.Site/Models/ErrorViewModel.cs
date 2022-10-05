fe96220b17d3aa15e22722f816bce1ce3844f3c8
unknown <faezehjafarpour75@gmail.com>
2022-10-05 18:29:37 +0330

    pkg

---
+++ RestaurantProject/Endpoint.Site/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Endpoint.Site.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
