f6bf0d518a57b4f0c703f6454b6f5c0dbcb55dc4
jafarpoor <faezehjafarpour75@gmail.com>
2022-11-21 07:17:31 +0330

    ordersservice

---
+++ RestaurantProject/Application/Orders/DTO/ListOrdersCustomerDataModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Orders.DTO
+{
+    public class ListOrdersCustomerDataModel
+    {
+        public string OrderCode { get; set; }
+        public string OrderStatus { get; set; }
+
+        public int PaymentAmount { get; set; }
+
+       public string PaymentDataTime {  get; set; }
+    }
+}
